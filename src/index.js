66d320ebcf6e46e0536748a83f263c161920bed7
Akshay <akshaymangutkar951@gmail.com>
2025-01-31 13:46:37 +0530

    added the cors

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import PORT from "./config/portConfig.js";
 import express from "express";
 import connectDB from "./config/dbConfig.js";
 import appRouter from "./router/index.js";
+import cors from "cors";
 
 const app = express();
+
+// Use CORS middleware to allow cross-origin requests
+app.use(cors()); // Enable CORS for all origins (can be configured if needed)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
